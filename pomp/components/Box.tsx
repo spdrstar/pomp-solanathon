130a674ea3d9134e538ef7c8eeb3a4558dfc732a
spdrstar <spdrstar@gmail.com>
2022-10-26 14:23:52 +0300

    styling changes

--- pomp/components/Box.tsx
+++ pomp/components/Box.tsx
@@ -6,10 +6,9 @@ import Image from "next/image"
 
 export default function Box() {
 
-  const [camera, setCamera] = useState(false);
+  const [camera, setCamera] = useState(true);
   const [image, setImage] = useState();
   const [isSelfieTaken, setIsSelfieTaken] = useState(false);
-  const [isShowVideo, setIsShowVideo] = useState(false);
   const [disabled, setDisabled] = useState(false);
   const [buttonText, setButtonText] = useState("Take your POMP")
 
@@ -30,13 +29,15 @@ export default function Box() {
       let stream = webcamRef.current.stream;
       const tracks = stream.getTracks();
       tracks.forEach(track => track.stop());
-      setIsShowVideo(false);
+      setCamera(false);
     }
   }
 
   const capture = React.useCallback(
     () => {
       let temp = 3;
+      setCamera(true);
+      setIsSelfieTaken(false);
       setDisabled(true);
       for(let i=0; i < 4; i++){
         setTimeout(() => {
@@ -50,10 +51,17 @@ export default function Box() {
         }, i*1000);
       }
       setTimeout(function(){
+        // Capture and Show Image
         setImage(webcamRef.current.getScreenshot());
         setIsSelfieTaken(true);
-        setIsShowVideo(false);
+
+        // Stop Camera
+        setCamera(false);
         stopCam();
+
+        // Set Button
+        setDisabled(false);
+        setButtonText("Retake POMP")
       }, 4000);
 
       },
@@ -61,19 +69,22 @@ export default function Box() {
     );
 
   return (
-    <Container xl>
+    <Container sm>
       <Card >
         <Card.Body>
-            <Row justify="center" align="center">
-              <Webcam style={{ borderRadius: "500px", position: "relative" }} audio={false} videoConstraints={videoConstraints} ref={webcamRef} onUserMedia={startCamera} />
-              <div style={{ position: "absolute"  }}>
-                <Image src="/fincPOMP.svg" alt="POMP overlay" width="500px" height="500px" />
-              </div>
+             <Row justify="center" align="center">
+              {camera && <> 
+                <Webcam style={{ borderRadius: "500px", position: "relative" }} audio={false} videoConstraints={videoConstraints} ref={webcamRef} onUserMedia={startCamera} />
+                <div style={{ position: "absolute"  }}>
+                  <Image src="/fincPOMP.svg" alt="POMP overlay" width="500px" height="500px" />
+                </div> </> }
+                {isSelfieTaken && <Image className="image-captured" src={image} width="500px" height="500px"></Image>}
             </Row>
         </Card.Body>
-        <Card.Footer>
-          <Button disabled={disabled} auto bordered css={{ px: "$13" }} onClick={capture}>{buttonText}</Button>
-        </Card.Footer>
+        <Row justify="center" align="center">
+          <Button disabled={disabled} bordered css={{ px: "$13", width: "1em" }} size="md" onClick={capture}>{buttonText}</Button>
+        </Row>
+        <Card.Footer></Card.Footer>
       </Card>
     </Container>
   );
